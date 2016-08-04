03b7d445e2793b996ecf2819110607aa3f0fbb73
Daniel Hayes <spdqbr@gmail.com>
2016-08-04 11:11:39 -0600

    Added log levels

--- template.sh
+++ template.sh
@@ -3,12 +3,27 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function error_handler() {
-      echo ""
-      echo "***************************************"
-      echo "Error occurred in script at line: ${1}."
-      echo "Line exited with status: ${2}"
-      echo "***************************************"
-      echo ""
+  echo ""
+  echo "***************************************"
+  echo "Error occurred in script at line: ${1}."
+  echo "Line exited with status: ${2}"
+  echo "***************************************"
+  echo ""
+}
+
+log() {
+  local level
+  level=$1
+  shift
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')] $level: $@"
+}
+
+info() {
+  log INFO $@
+}
+
+err() {
+  log ERROR $@ >&2
 }
 
 trap 'error_handler ${LINENO} $?' ERR
@@ -17,8 +32,10 @@ set -o errexit
 set -o errtrace
 set -o nounset
 
-echo "I'm good"
+info "I'm good"
 
-echo "script_dir = ${script_dir}"
+info "script_dir = ${script_dir}"
+
+err "I'm an error log message"
 
 test 1 -eq 0
